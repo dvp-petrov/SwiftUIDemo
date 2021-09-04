e54cf9f4a0a4921aa06cef528865d6280d728eb2
Dimitar Valentinov Petrov <dvp.petrov@gmail.com>
2021-09-04 11:46:43 +0300

    Initial scene button views implementation Added login button and create account buttons view. Removed unneeded `Font.` call when passing custom font. Edited TODOs in initial scene.

--- SwiftUIDemo/SwiftUIDemo/InitialSceneView.swift
+++ SwiftUIDemo/SwiftUIDemo/InitialSceneView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 /*
  TODO: refactor InitialSceneView
- 0. Implement buttons view.
  1. Extract fonts in constant reusable manner.
  2. Extract images in constant reusable manner.
  3. Extract colors in constant reusable manner.
  4. Refactor layout for smaller screens?
  5. Consider placing the logoImage(from top) and buttons(from bottom) with smaller padding if there are no safe area insets.
+ 6. Add actions to button views.
  **/
 
 struct InitialSceneView: View {
@@ -46,7 +46,7 @@ struct InitialSceneView: View {
                 Spacer()
                 buttons
             }
-            .padding(.bottom, 90)
+            .padding(.bottom, 94)
         }
         .ignoresSafeArea()
     }
@@ -69,10 +69,10 @@ struct InitialSceneView: View {
     private var texts: some View {
         VStack(spacing: 15) {
             Text("We are what we do")
-                .font(Font.custom("HelveticaNeue-Bold", size: 30))
+                .font(.custom("HelveticaNeue-Bold", size: 30))
                 .foregroundColor(Color(red: 63/255, green: 65/255, blue: 78/255, opacity: 1))
             Text("Thousand of people are using silent moon\nfor small meditations")
-                .font(Font.custom("HelveticaNeue-Light", size: 16))
+                .font(.custom("HelveticaNeue-Light", size: 16))
                 .foregroundColor(Color(red: 161/255, green: 164/255, blue: 178/255))
                 .lineSpacing(8)
         }
@@ -80,14 +80,28 @@ struct InitialSceneView: View {
     }
     
     private var buttons: some View {
-        VStack {
-            Text("Buttons")
-            Text("Buttons")
-            Text("Buttons")
-            Text("Buttons")
-            Text("Buttons")
+        VStack(spacing: 20) {
+            Button(action: {}) {
+                Text("SIGN UP")
+                    .foregroundColor(Color(red: 246/255, green: 241/255, blue: 251/255))
+            }
+            .frame(maxWidth: .infinity,
+                   maxHeight: 63)
+            .background(Color(red: 224/255, green: 142/255, blue: 253/255))
+            .clipShape(Capsule())
+            .padding(.horizontal, 20)
+            Button(action: {}) {
+                HStack(spacing: 0) {
+                    Text("ALREADY HAVE AN ACCOUNT? ")
+                        .foregroundColor(Color(red: 161/255, green: 164/255, blue: 178/255))
+                    Text("LOG IN")
+                        .foregroundColor(Color(red: 142/255, green: 151/255, blue: 253/255))
+                }
+            }
         }
+        .font(.custom("HelveticaNeue-Medium", size: 14))
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
